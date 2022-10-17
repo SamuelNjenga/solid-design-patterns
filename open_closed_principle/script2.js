7e3182443ca834b290b9c5e696e82024cb0b54dc
Samuel Njenga <snjenga704@gmail.com>
2022-10-17 12:03:37 +0300

    feat: create open_closed_principle directory and add files for supporting extension but not modification

---
+++ open_closed_principle/script2.js
@@ -0,0 +1,66 @@
+class BooleanQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log("1. True");
+    console.log("2. False");
+  }
+}
+
+class MultipleChoiceQuestion {
+  constructor(description, options) {
+    this.description = description;
+    this.options = options;
+  }
+
+  printQuestionChoices() {
+    this.options.forEach((option, index) => {
+      console.log(`${index + 1}. ${option}`);
+    });
+  }
+}
+
+class TextQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log("Answer: ______________");
+  }
+}
+
+class RangeQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log("Minimum: _____________");
+    console.log("Maximum: _____________");
+  }
+}
+
+function printQuiz(questions) {
+  questions.forEach((question) => {
+    console.log(question.description);
+    question.printQuestionChoices();
+    console.log("");
+  });
+}
+
+const questions = [
+  new BooleanQuestion("This video is useful."),
+  new MultipleChoiceQuestion("What is your favorite language?", [
+    "CSS",
+    "HTML",
+    "JS",
+    "Python",
+  ]),
+  new TextQuestion("Describe your favorite JS feature."),
+  new RangeQuestion("What is the speed limit in your city?"),
+];
+
+printQuiz(questions);
